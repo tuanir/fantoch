6af1f5afcaa3ef76ed50a3aa9fcc543cefc95c4c
TFR <tfr011@gmail.com>
2021-06-23 14:15:15 +0200

    Adding strategy

--- fantoch_ps/src/protocol/common/byzantine/byzquorum/mgridstrong.rs
+++ fantoch_ps/src/protocol/common/byzantine/byzquorum/mgridstrong.rs
@@ -2,6 +2,8 @@ use super::ByzQuorumSystem;
 use fantoch::id::ProcessId;
 use fantoch::HashSet;
 use simple_matrix::Matrix;
+use std::iter::FromIterator;
+use rand::distributions::{Distribution, Uniform};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct MGridStrong {
@@ -17,8 +19,47 @@ impl ByzQuorumSystem for MGridStrong {
     }
 
     fn get_quorum(&self) -> HashSet<ProcessId> {
-        let temp = HashSet::new();
-        temp
+        let mut q = HashSet::new();
+
+        // A quorum is `factor`*rows + `factor`columns
+        // We define `factor` in a way we can guarantee at least 3f+1 in 
+        // the intersection between any two quorums
+        let factor = (((((3*self.faults)/2) + 1) as f64).sqrt()).ceil() as usize;
+
+        println!("Factor: {}", factor);
+
+        let mut rng = rand::thread_rng();
+
+        // Be careful, matrix index starts at 0
+        let rows_die = Uniform::from(0..self.grid.rows());
+        let cols_die = Uniform::from(0..self.grid.cols());
+
+        
+        // FIXME: Note that we might take repeated cols/rows.
+        // Get random rows
+
+        //The following section can be improved (ofc), but will remain ugly for now.
+        for i in 0..factor {
+            let throw_rows = rows_die.sample(&mut rng);
+            println!("ROWS, i: {}, r: {}", i, throw_rows);
+            for e in self.grid.get_row(throw_rows).unwrap(){
+                 q.insert(*e);
+                 print!("{} ", e);
+             }
+             println!("");
+        }
+        // Get random columns
+        for i in 0..factor {
+            let throw_cols = cols_die.sample(&mut rng);
+            println!("COLS, i: {}, c: {}", i, throw_cols);
+            for e in self.grid.get_col(throw_cols).unwrap(){
+                 q.insert(*e);
+                 print!("{} ", e);
+            }
+             println!("");
+        }
+        println!("Size of Q: {}", q.len());
+        q
     }
 
     fn get_quorum_size(&self) -> usize {
@@ -28,12 +69,18 @@ impl ByzQuorumSystem for MGridStrong {
 
 impl MGridStrong {
 
-    // I'm assuming that the n provided in the config is correct
-    // Not the best approach...
+    // I'm assuming that 'n' provided in the config is correct
+    // TODO: properly sanitize when `n` and `f` are incompatible.
     pub fn from_set_to_matrix(procs: HashSet<ProcessId>) -> Matrix<ProcessId> {
         let n: usize = procs.len();
+        println!("Size of set: {}", n);
         let d = (n as f64).sqrt() as usize;
-        let res =  Matrix::from_iter(d, d, procs.into_iter());
+        println!("Dimension: {}", d);
+        //Equal view of the matrix
+        let mut v = Vec::from_iter(procs.clone());
+        v.sort();
+        let res =  Matrix::from_iter(d, d, v.into_iter());
         res
     }
+
 }
--- fantoch_ps/src/protocol/common/byzantine/byzquorum/mod.rs
+++ fantoch_ps/src/protocol/common/byzantine/byzquorum/mod.rs
@@ -4,8 +4,8 @@ mod mgridstrong;
 // Re-exports.
 pub use mgridstrong::MGridStrong;
 
-use fantoch::{HashMap, HashSet};
 use fantoch::id::ProcessId;
+use fantoch::{HashMap, HashSet};
 use serde::{Deserialize, Serialize};
 use std::fmt::Debug;
 
@@ -13,26 +13,42 @@ use std::fmt::Debug;
 // quoracle-style analysis.
 
 pub trait ByzQuorumSystem: Debug + Clone {
-
     /// Create a new `ByzQuorumSystem` instance.
     /// All processes in the system must have been passed as a parameter
     fn new(system_processes: HashSet<ProcessId>, faults: usize) -> Self;
 
     /// Finds a quorum according to some strategy
-    /// that abides to a certain intersection rule. 
+    /// that abides to a certain intersection rule.
     fn get_quorum(&self) -> HashSet<ProcessId>;
 
     fn get_quorum_size(&self) -> usize;
 }
 
+//cargo test --lib byzantine -- --show-output
+//remember to test everything in byzantine
+//cargo test protocol::common::byzantine::byzquorum::tests::build_mgrid_strong -- --exact --show-output
+
 #[cfg(test)]
 mod tests {
-    use simple_matrix::Matrix;
+    use super::*;
     use fantoch::id::ProcessId;
-    use fantoch::{HashMap, HashSet};
-    
+    use fantoch::HashSet;
+    use std::iter::FromIterator;
     #[test]
     fn build_mgrid_strong() {
-       println!("test")
+        let p: HashSet<ProcessId> = HashSet::from_iter(0..49);
+        let s = MGridStrong::new(p, 1);
+
+        for i in 0..s.grid.rows() {
+            let a = s.grid.get_row(i);
+            for j in a.unwrap() {
+                print!("{} ", j);
+            }
+            println!("");
+        }
+
+        println!("grid 7x7");
+
+        s.get_quorum();
     }
 }
